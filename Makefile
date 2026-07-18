3f95787a47d3af3b43977c38fbfe343a578335ea
PizzaC4t <pizzac4t@proton.me>
2026-07-18 13:20:17 -0500

    add support for fat 12

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-gcc:
-	x86_64-w64-mingw32-gcc efi/main.c \
-	-std=c17 \
-	-Wall \
-	-Wextra \
-	-Wpedantic \
-	-ffreestanding \
-	-nostdlib \
-	-Wl, --subsystem,10 \
-	-e efi_main \
-	-o BOOTX64.EFI
+SRC_DIR=src
+BUILD_DIR=build
+LEGACY_DIR=src/boot/Legacy
+
+legacy:
+
+	mkdir -p $(BUILD_DIR)
+	/usr/bin/nasm $(LEGACY_DIR)/boot.asm -f bin -o $(BUILD_DIR)/bootLegacy.bin
+
+	/usr/bin/dd if=/dev/zero of=$(BUILD_DIR)/floppy.img bs=512 count=2880
+	mkfs.fat -F 12 -n "PZZOS" $(BUILD_DIR)/floppy.img
+	/usr/bin/dd if=$(BUILD_DIR)/bootLegacy.bin of=$(BUILD_DIR)/floppy.img conv=notrunc
+	/usr/bin/truncate -s 1440k $(BUILD_DIR)/floppy.img
+#NOTE add kernel section
--- src/boot/Legacy/boot.asm
+++ src/boot/Legacy/boot.asm
@@ -6,6 +6,32 @@ _start:
   nop
   times 33 db 0 ;correct bytes to avoid BPB to corrupt code
 
+  ;
+  ;Fat 12 header
+  ;
+  bdb_oem:                   db 'MSWIN4.1' ;8 bytes
+  bdb_byte_per_sector:       dw 512
+  bdb_sectores_per_cluster:  db 1
+  bdb_reserved_sectore:      dw 1
+  bdb_fat_count:             db 2
+  bdb_dir_entries_count:     dw 0E0h
+  bdb_total_sectors:         dw 2880 ;1.4mb I think
+  bdb_media_descriptor_type: db 0E0h ; F0 = 3.5" floppy
+  bdb_sectores_per_fat:      dw 9
+  bdb_sectors_per_track:     dw 18
+  bdb_heads:                 dw 2
+  bdb_hidedn_sectors:        dd 0
+  bdb_large_sector_count:    dd 0
+
+  ;Extended boot record
+  ebr_drive_number:          db 0 ;for floppy
+                             db 0 ;reserved
+  ebr_signature:             dw 28h
+  ;ebr_volume_id:            db 12h   no serial number for now...
+  ebr_volume_label:          db 'PIZZOS     ' ;11 bytes, see what I did there?
+  ebr_system_id:             db 'FAT12   '
+
+
 start:
   jmp 0x7c0:run
 
