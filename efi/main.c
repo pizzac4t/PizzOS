2041d6517c6eea5b0e5e400ccc283505e5be600c
pizzac4t <pizzac4t@proton.me>
2026-08-06 23:57:10 -0500

    removed old bootloader folder

--- efi/main.c
+++ efi/main.c
@@ -1,11 +1,4 @@
 #include "headers/efi.h"
 #include "headers/efilib.h"
 
-EFI_STATUS
-EFIAPI
-
-efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  InitializeLib(ImageHandle, SystemTable);
-  Print(L"Hello, world!\n");
-  return EFI_SUCCESS;
-}
+E
